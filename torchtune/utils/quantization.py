17c39cee5b15cbc50fb0b70a62677bf4b5362a22
andrewor14 <andrewor14@gmail.com>
2024-05-13 20:04:20 -0700

    Add QAT support for distributed finetuning
    Summary: This commit adds the option to run quantization-aware
    training (QAT) during finetuning. QAT refers to "fake quantizing"
    the weights and activations during training, which performs the
    following transformation on the inputs but still keeps all
    intermediate values in floating point:
    
    ```
    x_q = clamp((x_bf16 / scale) + zp)
    x_fq = (x_q - zp) * scale
    ```
    
    Currently only 8-bit per token dynamic activations + 4-bit grouped
    per channel weights (8da4w) is supported. Users can enable this by
    specifying a QAT quantizer in their config files:
    
    ```
    tune run --nnodes 1 --nproc_per_node 8 qat_distributed --config qat
    ```
    
    Test Plan:
    
    Initial results for Llama2 demonstrate that QAT is able to
    recover the loss in accuracy from quantization by about half
    for some tasks (last two rows):
    
    |                 | hellaswag |          |     wikitext    |                 |               | arc_easy |          | arc_challenge |          |
    |-----------------|:---------:|----------|:---------------:|-----------------|---------------|:--------:|----------|:-------------:|----------|
    |                 |    acc    | acc_norm | word_perplexity | byte_perplexity | bits_per_byte |    acc   | acc_norm |      acc      | acc_norm |
    | No quantization |  59.659%  |  76.927% |      12.183     |      1.596      |     0.674     |  76.010% |  72.054% |    48.720%    |  47.867% |
    | PTQ             |  57.150%  |  74.945% |      12.995     |      1.615      |     0.692     |  75.968% |  70.118% |    46.416%    |  45.904% |
    | QAT (bf16)      |  58.435%  |  76.190% |      12.200     |      1.596      |     0.675     |  76.810% |  72.180% |    47.270%    |  47.184% |
    | QAT (quantized) |  58.504%  |  76.170% |      12.199     |      1.596      |     0.675     |  76.431% |  71.928% |    47.184%    |  48.123% |
    | PTQ degradation |  -2.509%  |  -1.982% |      0.812      |      0.019      |     0.018     |  -0.042% |  -1.936% |    -2.304%    |  -1.963% |
    | QAT degradation |  -1.155%  |  -0.757% |      0.016      |      0.000      |     0.001     |  0.421%  |  -0.126% |    -1.536%    |  0.256%  |

--- torchtune/utils/quantization.py
+++ torchtune/utils/quantization.py
@@ -18,9 +18,12 @@ from torchao.quantization.quant_api import (
 # is only available after 2.3 so we have to guard the pytorch versions to decide
 # the list of supported quantizers
 try:
-    from torchao.quantization.utils import TORCH_VERSION_AFTER_2_3
+    from torchao.quantization.utils import (
+        TORCH_VERSION_AFTER_2_3,
+        TORCH_VERSION_AFTER_2_4,
+    )
 except Exception:
-    from torchao.utils import TORCH_VERSION_AFTER_2_3
+    from torchao.utils import TORCH_VERSION_AFTER_2_3, TORCH_VERSION_AFTER_2_4
 
 __all__ = [
     "Int4WeightOnlyQuantizer",
@@ -43,6 +46,8 @@ _quantizer_to_mode = {
     Int8WeightOnlyQuantizer: "8w",
     Int4WeightOnlyGPTQQuantizer: "4w-gptq",
 }
+_quantizer_mode_to_disable_fake_quant = {}
+_quantizer_mode_to_enable_fake_quant = {}
 
 
 if TORCH_VERSION_AFTER_2_3:
@@ -50,6 +55,19 @@ if TORCH_VERSION_AFTER_2_3:
 
     __all__.append("Int8DynActInt4WeightQuantizer")
     _quantizer_to_mode[Int8DynActInt4WeightQuantizer] = "8da4w"
+
+
+if TORCH_VERSION_AFTER_2_4:
+    from torchao.quantization.prototype.qat import (
+        disable_8da4w_fake_quant,
+        enable_8da4w_fake_quant,
+        Int8DynActInt4WeightQATQuantizer,
+    )
+
+    __all__.append("Int8DynActInt4WeightQATQuantizer")
+    _quantizer_to_mode[Int8DynActInt4WeightQATQuantizer] = "8da4w-qat"
+    _quantizer_mode_to_disable_fake_quant["8da4w-qat"] = disable_8da4w_fake_quant
+    _quantizer_mode_to_enable_fake_quant["8da4w-qat"] = enable_8da4w_fake_quant
 
 
 def get_quantizer_mode(quantizer: Optional[Callable]) -> Optional[str]:
@@ -65,3 +83,19 @@ def get_quantizer_mode(quantizer: Optional[Callable]) -> Optional[str]:
         Optional[str]: The quantization mode.
     """
     return _quantizer_to_mode.get(type(quantizer), None)
+
+
+def _get_disable_fake_quant(quantizer_mode: str) -> Callable:
+    """Given a quantizer mode, return the corresponding function for disabling fake
+    quantize in a model prepared by the quantizer.
+    If the quantizer is not recognized as a known QAT quantizer, return None.
+    """
+    return _quantizer_mode_to_disable_fake_quant.get(quantizer_mode, None)
+
+
+def _get_enable_fake_quant(quantizer_mode: str) -> Callable:
+    """Given a quantizer mode, return the corresponding function for enabling fake
+    quantize in a model prepared by the quantizer.
+    If the quantizer is not recognized as a known QAT quantizer, return None.
+    """
+    return _quantizer_mode_to_enable_fake_quant.get(quantizer_mode, None)
