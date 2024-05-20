f8bb13fd3b46c67ffb80c79d79ab705744cf7234
andrewor14 <andrewor14@gmail.com>
2024-05-20 16:50:32 -0700

    add run script for devgpu023
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ temp_script2.sh
@@ -0,0 +1,108 @@
+# ============== #
+#  on devgpu023  #
+# ============== #
+
+# 5/20/24 (wikitext-2-v1)
+
+cd /home/andrewor/local/ao
+git checkout qat-test
+cd /home/andrewor/local/torchtune
+
+export TIMESTAMP=`date +%s`
+export LLAMA_VERSION=2
+export ENABLE_FAKE_QUANT_STEP=1000
+export BATCH_SIZE=4
+export NUM_EPOCHS=3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+echo -e "\n\n\n=== Running full ==="
+RUN_TAG="8da4w" ./run_it.sh full
+echo -e "\n\n\n=== Running QAT ==="
+RUN_TAG="8da4w" ./run_it.sh qat
+
+echo -e "\n\n\n=== Running eval on vanilla 8da4w ==="
+EXP_DIR="/home/andrewor/local/logs/tune/qat_llama2_${TIMESTAMP}_8da4w"
+CUDA_VISIBLE_DEVICES=0,1 CHECKPOINT_FILES="[hf_model_0001_0.pt, hf_model_0002_0.pt]" RUN_TAG="e0" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=2,3 CHECKPOINT_FILES="[hf_model_0001_1.pt, hf_model_0002_1.pt]" RUN_TAG="e1" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=4,5 CHECKPOINT_FILES="[hf_model_0001_2.pt, hf_model_0002_2.pt]" RUN_TAG="e2" ./eval_it.sh $EXP_DIR &
+wait
+
+echo -e "\n\n\n=== Running eval on vanilla 8da4w (baseline) ==="
+EXP_DIR="/home/andrewor/local/logs/tune/full_llama2_${TIMESTAMP}_8da4w"
+CUDA_VISIBLE_DEVICES=0,1 CHECKPOINT_FILES="[hf_model_0001_0.pt, hf_model_0002_0.pt]" RUN_TAG="8da4w_e0" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=2,3 CHECKPOINT_FILES="[hf_model_0001_1.pt, hf_model_0002_1.pt]" RUN_TAG="8da4w_e1" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=4,5 CHECKPOINT_FILES="[hf_model_0001_2.pt, hf_model_0002_2.pt]" RUN_TAG="8da4w_e2" ./eval_it.sh $EXP_DIR &
+wait
+
+
+# Try 3b weight only
+
+cd /home/andrewor/local/ao
+git checkout 3b-weight-only
+cd /home/andrewor/local/torchtune
+
+echo -e "\n\n\n=== Running QAT 3w ==="
+RUN_TAG="3w" ./run_it.sh qat
+
+echo -e "\n\n\n=== Running eval on 3w ==="
+EXP_DIR="/home/andrewor/local/logs/tune/qat_llama2_${TIMESTAMP}_3w"
+CUDA_VISIBLE_DEVICES=0,1 CHECKPOINT_FILES="[hf_model_0001_0.pt, hf_model_0002_0.pt]" RUN_TAG="e0" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=2,3 CHECKPOINT_FILES="[hf_model_0001_1.pt, hf_model_0002_1.pt]" RUN_TAG="e1" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=4,5 CHECKPOINT_FILES="[hf_model_0001_2.pt, hf_model_0002_2.pt]" RUN_TAG="e2" ./eval_it.sh $EXP_DIR &
+wait
+
+echo -e "\n\n\n=== Running eval on 3w (baseline) ==="
+EXP_DIR="/home/andrewor/local/logs/tune/full_llama2_${TIMESTAMP}_8da4w"
+CUDA_VISIBLE_DEVICES=0,1 CHECKPOINT_FILES="[hf_model_0001_0.pt, hf_model_0002_0.pt]" RUN_TAG="3w_e0" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=2,3 CHECKPOINT_FILES="[hf_model_0001_1.pt, hf_model_0002_1.pt]" RUN_TAG="3w_e1" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=4,5 CHECKPOINT_FILES="[hf_model_0001_2.pt, hf_model_0002_2.pt]" RUN_TAG="3w_e2" ./eval_it.sh $EXP_DIR &
+wait
+
+
+# Try 2b weight only
+
+cd /home/andrewor/local/ao
+git checkout 2b-weight-only
+cd /home/andrewor/local/torchtune
+
+echo -e "\n\n\n=== Running QAT 2w ==="
+RUN_TAG="2w" ./run_it.sh qat
+
+echo -e "\n\n\n=== Running eval on 2w ==="
+EXP_DIR="/home/andrewor/local/logs/tune/qat_llama2_${TIMESTAMP}_2w"
+CUDA_VISIBLE_DEVICES=0,1 CHECKPOINT_FILES="[hf_model_0001_0.pt, hf_model_0002_0.pt]" RUN_TAG="e0" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=2,3 CHECKPOINT_FILES="[hf_model_0001_1.pt, hf_model_0002_1.pt]" RUN_TAG="e1" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=4,5 CHECKPOINT_FILES="[hf_model_0001_2.pt, hf_model_0002_2.pt]" RUN_TAG="e2" ./eval_it.sh $EXP_DIR &
+wait
+
+echo -e "\n\n\n=== Running eval on 2w (baseline) ==="
+EXP_DIR="/home/andrewor/local/logs/tune/full_llama2_${TIMESTAMP}_8da4w"
+CUDA_VISIBLE_DEVICES=0,1 CHECKPOINT_FILES="[hf_model_0001_0.pt, hf_model_0002_0.pt]" RUN_TAG="2w_e0" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=2,3 CHECKPOINT_FILES="[hf_model_0001_1.pt, hf_model_0002_1.pt]" RUN_TAG="2w_e1" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=4,5 CHECKPOINT_FILES="[hf_model_0001_2.pt, hf_model_0002_2.pt]" RUN_TAG="2w_e2" ./eval_it.sh $EXP_DIR &
+wait
+
+
+# Try 2b weight only gs32
+
+cd /home/andrewor/local/ao
+git checkout 2b-weight-only
+cd /home/andrewor/local/torchtune
+
+echo -e "\n\n\n=== Running QAT 2w gs32 ==="
+RUN_TAG="2w_gs32" ./run_it.sh qat
+
+echo -e "\n\n\n=== Running eval on 2w gs32 ==="
+EXP_DIR="/home/andrewor/local/logs/tune/qat_llama2_${TIMESTAMP}_2w_gs32"
+CUDA_VISIBLE_DEVICES=0,1 CHECKPOINT_FILES="[hf_model_0001_0.pt, hf_model_0002_0.pt]" RUN_TAG="e0" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=2,3 CHECKPOINT_FILES="[hf_model_0001_1.pt, hf_model_0002_1.pt]" RUN_TAG="e1" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=4,5 CHECKPOINT_FILES="[hf_model_0001_2.pt, hf_model_0002_2.pt]" RUN_TAG="e2" ./eval_it.sh $EXP_DIR &
+wait
+
+echo -e "\n\n\n=== Running eval on 2w gs32 (baseline) ==="
+EXP_DIR="/home/andrewor/local/logs/tune/full_llama2_${TIMESTAMP}_8da4w"
+CUDA_VISIBLE_DEVICES=0,1 CHECKPOINT_FILES="[hf_model_0001_0.pt, hf_model_0002_0.pt]" RUN_TAG="2w_gs32_e0" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=2,3 CHECKPOINT_FILES="[hf_model_0001_1.pt, hf_model_0002_1.pt]" RUN_TAG="2w_gs32_e1" ./eval_it.sh $EXP_DIR &
+CUDA_VISIBLE_DEVICES=4,5 CHECKPOINT_FILES="[hf_model_0001_2.pt, hf_model_0002_2.pt]" RUN_TAG="2w_gs32_e2" ./eval_it.sh $EXP_DIR &
+wait
+
